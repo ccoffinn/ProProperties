8ca16930f5c6de9b5b7d4b1a30a5def32e13fab7
ccoffinn <159832743+ccoffinn@users.noreply.github.com>
2025-02-14 14:37:38 +0000

    Basic version of website

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
 <head>
     <title>Test Page</title>
     <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/searchBar.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=search">
 </head>
 
 <body>
 
 <header>
-    <!-- temp title -->
-    <h1>ProProperties</h1>
+    <div class="container">
+        <!-- temp title -->
+        <div class="logoTemp">
+            <h1>ProProperties</h1>
+        </div>
+        <!--Action for functionality, empty for now-->
+        <!--Search Bar Reference: https://www.youtube.com/watch?v=f6ocDCkCmhM -->
+        <form action="">
+            <div class="search">
+                <span class="search-icon material-symbols-outlined">search</span>
+                <!--Placeholder is default text for search bar-->
+                <input class="search-input" type="search" placeholder="Search">
+            </div>
+        </form>
+        <hr>
+    </div>
 </header>
 
 <main>
@@ -22,6 +38,7 @@ $houses = json_decode($housesJson, true);
 <div class=listings>
     <!-- loop to display listings on home page -->
     <?php foreach ($houses as $house) { ?>
+        <a href="ProductDescription.html">
         <div class=house>
             <img src="/images/<?php echo $house['filename']; ?>" class = houseImage alt="Photo of House">
             <h2 class="addressLine"><?php echo $house['address']; ?>, <?php echo $house['price'] ?></h2>
@@ -41,6 +58,7 @@ $houses = json_decode($housesJson, true);
                 </h5>
             </div>
         </div>
+        </a>
     <?php } ?>
 </div>
 </main>
