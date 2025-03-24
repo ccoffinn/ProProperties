6fa1be45c7b330ed5791be378e51f1bf21abb098
ccoffinn <159832743+ccoffinn@users.noreply.github.com>
2025-03-24 00:37:58 +0000

    fixed templates

--- public/templates/login_header.php
+++ public/templates/login_header.php
@@ -1,4 +1,4 @@
-<? 
+<?php
 
 // If user isn;t logged in and the current web page isnt the login page redirect to the login page
 if (!isset($_SESSION['Active']) && basename($_SERVER['PHP_SELF']) !== 'login.php' && basename($_SERVER['PHP_SELF']) !== 'signup.php') {
